6104c68c093af25442b358ce6c30d5f7ba7abd34
cbpark <cbpark@gmail.com>
2020-02-05 00:29:33 +0900

    Update qmass.hs

--- src/qmass.hs
+++ src/qmass.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import HEP.Data.AlphaS (alphasQ, mkAlphaS)
+
 main :: IO ()
-main = putStrLn "Hello, world!"
+main = do
+    let mt = 173.0
+        mZ = 91.188
+        aMZ = 0.118
+
+    as <- mkAlphaS mt mZ aMZ
+
+    let printAlphaS q = alphasQ as q >>= print
+    mapM_ printAlphaS [4.18, mZ, mt]
