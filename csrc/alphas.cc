e233278774f4e34a8845981c4cbfa03c92a77d39
cbpark <cbpark@gmail.com>
2020-02-05 16:08:36 +0900

    Fix alphas.cc

--- csrc/alphas.cc
+++ csrc/alphas.cc
@@ -3,11 +3,14 @@
 
 extern "C" {
 CAlphaS *mkAlphaS(double mt, double mz, double alpha) {
+    // See https://lhapdf.hepforge.org/_2tests_2testalphas_8cc-example.html
     LHAPDF::AlphaS *as = new LHAPDF::AlphaS_ODE();
     as->setOrderQCD(5);
-    as->setQuarkMass(6, mt);
     as->setMZ(mz);
     as->setAlphaSMZ(alpha);
+    as->setQuarkMass(4, 1.27);
+    as->setQuarkMass(5, 4.18);  // without this, alpha_s will blow up.
+    as->setQuarkMass(6, mt);
 
     return reinterpret_cast<CAlphaS *>(as);
 }
